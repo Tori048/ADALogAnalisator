710d3ea1ccc44535ae6e1abf33ef06b246aebabf
Tori048 <dimka048@gmail.com>
2021-12-01 22:57:57 +0300

    Начало проекта

--- ADALogAnalisator/Form1.cs
+++ ADALogAnalisator/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace ADALogAnalisator
 {
@@ -15,6 +16,18 @@ namespace ADALogAnalisator
         public Form1()
         {
             InitializeComponent();
+            //openFileDialog1.Filter = "Text files(*.txt)|*.txt|All files(*.*)|*.*";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            // получаем выбранный файл
+            string filename = openFileDialog1.FileName;
+            // читаем файл в строку
+            string fileText = File.ReadAllText(filename);
+            MessageBox.Show("Файл открыт");
         }
     }
 }
